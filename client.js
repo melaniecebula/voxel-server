a51e2aea9be7be48afcff8fc5daf609467345a4d
Melanie Cebula <melaniecebula@berkeley.edu>
2013-03-09 11:36:24 -0800

    max changes

--- client.js
+++ client.js
@@ -105,10 +105,9 @@ function createGame(options) {
   // rescue(game)
   showPlayer = function() {  //showPlayer already defined in global scope
     var createPlayer = player(game)
-    if (team == "blueTeam") viking = createPlayer('blueSkin.png')
-    if (team == "redTeam") viking = createPlayer('redSkin.png')
-    else viking = createPlayer('viking.png')
-    //viking = createPlayer('skin2.png')  //TO DO: when a new player connects, randomly assign them to team + give them that skin
+    console.log('team!', team)
+    if (team === "blueTeam") viking = createPlayer('blueSkin.png')
+    if (team === "redTeam") viking = createPlayer('redSkin.png')
     viking.moveTo(options.startingPosition)
     viking.possess()
     game.controls.on('data', function(state) {
@@ -236,10 +235,8 @@ function updatePlayerPosition(id, update) {
   var pos = update.position
   var player = players[id]
   if (!player) {
-    if(team=='blueTeam') playerSkin=skin(game.THREE, 'blueSkin.png')
-    if(team=='redTeam') playerSkin=skin(game.THREE, 'redSkin.png')
-    else playerSkin=skin(game.THREE, 'viking.png')
-    //var playerSkin = skin(game.THREE, 'skin2.png') //edit for teama.png and teamb.png (shows players moving smoothly)
+    if (update.team === "blueTeam") playerSkin = skin(game.THREE, 'blueSkin.png')
+    if (update.team === "redTeam") playerSkin = skin(game.THREE, 'redSkin.png')
     var playerMesh = playerSkin.mesh
     players[id] = playerSkin
     playerMesh.children[0].position.y = 10
